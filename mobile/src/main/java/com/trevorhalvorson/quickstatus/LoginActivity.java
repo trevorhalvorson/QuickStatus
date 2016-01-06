0c0aa10d241da82090bed9c2c91573faae89ee3b
Trevor Halvorson <trevor.halvorson@gmail.com>
2016-01-06 13:00:31 -0800

    Implement Facebook logout in MainActivity menu, update Login screen UI

--- mobile/src/main/java/com/trevorhalvorson/quickstatus/LoginActivity.java
+++ mobile/src/main/java/com/trevorhalvorson/quickstatus/LoginActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -15,15 +14,13 @@ import com.facebook.login.widget.LoginButton;
 public class LoginActivity extends AppCompatActivity {
 
     private static final String PERMISSIONS = "publish_actions";
-    
+
     private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         callbackManager = CallbackManager.Factory.create();
 
@@ -33,6 +30,7 @@ public class LoginActivity extends AppCompatActivity {
                 new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
+                        // TODO: Check if permission was granted before starting MainActivity
                         Intent successIntent = new Intent(LoginActivity.this, MainActivity.class);
                         successIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                                 .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -41,6 +39,7 @@ public class LoginActivity extends AppCompatActivity {
 
                     @Override
                     public void onCancel() {
+                        // TODO: Switch to DialogFragment to show longer message explaining permissions
                         Snackbar.make(findViewById(R.id.login_layout), R.string.login_cancel_text, Snackbar.LENGTH_LONG).show();
                     }
 
@@ -49,12 +48,10 @@ public class LoginActivity extends AppCompatActivity {
                         Snackbar.make(findViewById(R.id.login_layout), R.string.login_error_text, Snackbar.LENGTH_LONG).show();
                     }
                 });
-
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
-
 }
--- mobile/src/main/java/com/trevorhalvorson/quickstatus/MainActivity.java
+++ mobile/src/main/java/com/trevorhalvorson/quickstatus/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -56,5 +58,20 @@ public class MainActivity extends AppCompatActivity {
         loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(loginIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                logout();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
