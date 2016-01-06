7e5a8ead2615f77e6e63a3c93d45b1ca8b2d7e19
Trevor Halvorson <trevor.halvorson@gmail.com>
2016-01-06 03:08:10 -0800

    Implement displaying post request result as notification to mobile and wearable

--- mobile/src/main/java/com/trevorhalvorson/quickstatus/MobileListenerService.java
+++ mobile/src/main/java/com/trevorhalvorson/quickstatus/MobileListenerService.java
@@ -1,12 +1,17 @@
 package com.trevorhalvorson.quickstatus;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 
 import com.facebook.AccessToken;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.HttpMethod;
+import com.facebook.Profile;
 import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.WearableListenerService;
 
@@ -17,7 +22,6 @@ public class MobileListenerService extends WearableListenerService {
 
     private static final String TAG = MobileListenerService.class.getSimpleName();
     private static final String WEARABLE_DATA_PATH = "/wearable/data/path";
-    public static final String MESSAGE_EXTRA = "message_extra";
 
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
@@ -32,8 +36,30 @@ public class MobileListenerService extends WearableListenerService {
                     HttpMethod.POST,
                     new GraphRequest.Callback() {
                         public void onCompleted(GraphResponse response) {
-                            Log.i(TAG, "onCompleted: " + response.getRawResponse());
-                            // Make notification with status of post
+                            String title = "Facebook post successful";
+                            String content = "Your Facebook status has been updated.";
+
+                            if (response.getError() != null) {
+                                title = "Error publishing post";
+                                content = "An error occurred while updating your status. Please try again";
+                            }
+
+                            int notificationId = 0;
+                            String facebookUrl = "https://www.facebook.com/" + Profile.getCurrentProfile().getId();
+                            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(facebookUrl));
+                            PendingIntent pendingIntent =
+                                    PendingIntent.getActivity(MobileListenerService.this, 0, intent, 0);
+
+                            NotificationCompat.Builder builder =
+                                    new NotificationCompat.Builder(MobileListenerService.this)
+                                            .setSmallIcon(R.mipmap.ic_launcher)
+                                            .setContentTitle(title)
+                                            .setContentText(content)
+                                            .addAction(R.drawable.ic_person, getString(R.string.notification_action_text), pendingIntent);
+
+                            NotificationManagerCompat manager =
+                                    NotificationManagerCompat.from(MobileListenerService.this);
+                            manager.notify(notificationId, builder.build());
                         }
                     }
             ).executeAsync();
